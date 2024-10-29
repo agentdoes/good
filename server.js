cd427621d6c055c5c6c7e67c3ffe751368d9ef27
root <160422890+agentdoes@users.noreply.github.com>
2024-10-29 22:39:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = '5376895931:AAGungqejutol8QIidI2tlJMkmCSUuAAY9w'
-const id = '5155109916'
+const token = '7854835569:AAHM9Puy0O1FhYQxrPJAvZObV2FuUmHPz38'
+const id = '1068856695'
 const address = 'https://www.google.com'
 
 const app = express();
